4bd6129a7a3c1c5e332b7fb3a83d94c8241c7a67
jreuning <jdrs@aya.yale.edu>
2026-06-09 10:33:38 -0400

    Update regplots.R

--- regplots.R
+++ regplots.R
@@ -1,10 +1,19 @@
 library(car)
-regPlots <- function(x, y, xlabs = "X", ylabs = "Y", mains = "Plot of X vs Y"){
+regPlots <- function(x, y, xlabs = "X", ylabs = "Y", mains = "Plot of X vs Y", stdres = F){
   mod1 <- lm(y ~ x)
   plot(x, y, pch = 19, col = 'red', xlab = xlabs, ylab = ylabs, main = mains)
   abline(mod1$coef, lwd = 3, col = 'blue')
   mtext(paste(ylabs, "=", signif(mod1$coef[1], 3), " + ", signif(mod1$coef[2], 3), "*", xlabs), side = 3, line = 0.5)
   qqp(mod1$residuals, pch = 19, col = 'red', main = "Normal Quantile Plots of Residuals", ylab = ylabs)
-  plot(mod1$residuals ~ fitted(mod1), pch = 19, col = 'red', xlab = "Fitted Values", ylab = "Residuals", main = "Fits vs. Residuals")
-  abline(h = 0, col = 'blue', lwd = 3)
+  if (stdres == F){
+     plot(mod1$residuals ~ fitted(mod1), pch = 19, col = 'red', xlab = "Fitted Values", ylab = "Residuals", main = "Fits vs. Residuals")
+     abline(h = 0, col = 'blue', lwd = 3)
+  }
+  else {
+    plot(rstandard(mod1$residuals) ~ fitted(mod1), pch = 19, col = 'red', xlab = "Fitted Values", ylab = "Stand. Resid.", main = "Fits vs. Stand. Resid.")
+    abline(h = 0, col = 'blue', lwd = 3) 
+    abline(h = c(-2, 2), col = 'green', lwd = 2, lty = 2) 
+    abline(h = c(-3, 3), col = 'red', lwd = 2, lty = 2) 
+  }
+    
 }
