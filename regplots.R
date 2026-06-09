0530418057b278a9d7fb2dae40f5cbd9bb1c1772
jreuning <jdrs@aya.yale.edu>
2026-06-09 12:08:33 -0400

    Update regplots.R

--- regplots.R
+++ regplots.R
@@ -1,7 +1,8 @@
 library(car)
 regPlots <- function(x, y, xlabs = "X", ylabs = "Y", mains = "Plot of X vs Y", stdres = F){
-  mod1 <- lm(y ~ x)
-  plot(x, y, pch = 19, col = 'red', xlab = xlabs, ylab = ylabs, main = mains)
+  regdata <- na.omit(data.frame(x, y))
+  mod1 <- lm(y ~ x, data = regdata)
+  plot(x, y, data = regdata, pch = 19, col = 'red', xlab = xlabs, ylab = ylabs, main = mains)
   abline(mod1$coef, lwd = 3, col = 'blue')
   mtext(paste(ylabs, "=", signif(mod1$coef[1], 3), " + ", signif(mod1$coef[2], 3), "*", xlabs), side = 3, line = 0.5)
   qqp(mod1$residuals, pch = 19, col = 'red', main = "Normal Quantile Plots of Residuals", ylab = ylabs)
@@ -14,6 +15,7 @@ regPlots <- function(x, y, xlabs = "X", ylabs = "Y", mains = "Plot of X vs Y", s
     abline(h = 0, col = 'blue', lwd = 3) 
     abline(h = c(-2, 2), col = 'green', lwd = 2, lty = 2) 
     abline(h = c(-3, 3), col = 'red', lwd = 2, lty = 2) 
+    text(fitted(mod1)[abs(rstandard(mod1)) > 2], rstandard(mod1)[abs(rstandard(mod1)) > 2], rownames(regdata)[abs(rstandard(mod1)) > 2])
   }
     
 }
