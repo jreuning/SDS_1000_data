e434ec6c2e26689df98667773c07a9c3bbf8f7fa
jreuning <jdrs@aya.yale.edu>
2026-06-13 11:37:14 -0400

    Update regplots.R

--- regplots.R
+++ regplots.R
@@ -1,6 +1,6 @@
 library(car)
 library(PerformanceAnalytics)
-regPlots <- function(x, y, xlabs = "X", ylabs = "Y", mains = "Plot of X vs Y", stdres = F, resplots = T, bands = F, xlims = c(NA, NA)) {
+regPlots <- function(x, y, xlabs = "X", ylabs = "Y", mains = "Plot of X vs Y", fitplot = T, stdres = F, resplots = T, bands = F, xlims = c(NA, NA)) {
   regdata <- na.omit(data.frame(x, y))
   mod1 <- lm(y ~ x, data = regdata)
   ylims <-  range(regdata$y)
@@ -15,26 +15,23 @@ regPlots <- function(x, y, xlabs = "X", ylabs = "Y", mains = "Plot of X vs Y", s
     ylims <- range(predbands1)
   }
   if (is.na(xlims[1])) {  xlims <- range(regdata$x)}  
-  plot(y ~ x, data = regdata, pch = 19, col = 'red', 
+  if (fitplot == T){
+    plot(y ~ x, data = regdata, pch = 19, col = 'red', 
        xlab = xlabs, 
        ylab = ylabs, 
        main = mains,
        ylim = ylims,
        xlim = xlims)
-  abline(mod1$coef, lwd = 3, col = 'green')
-  mtext(paste(ylabs, "=", signif(mod1$coef[1], 3), " + ", signif(mod1$coef[2], 3), "*", xlabs), side = 3, line = 0.5)
-  if (bands == T){
-    
+   abline(mod1$coef, lwd = 3, col = 'green')
+   mtext(paste(ylabs, "=", signif(mod1$coef[1], 3), " + ", signif(mod1$coef[2], 3), "*", xlabs), side = 3, line = 0.5)
+   if (bands == T){
     lines(newX, confbands1[, "lwr"], lty = 2, col = 'red',lwd = 2)
     lines(newX, confbands1[, "upr"], lty = 2, col = 'red',lwd = 2)
-    
     lines(newX, predbands1[, "lwr"], lty = 3, col = 'blue',lwd = 2)
     lines(newX, predbands1[, "upr"], lty = 3, col = 'blue',lwd = 2)
-    
     legend("topleft", c("Conf. Bands", "Pred. Bands"), lty = c(2,3), lwd = 3, col = c("red", "blue"))  
-    
+   } 
   }
-  
   if (resplots == T){
     qqp(mod1$residuals, pch = 19, col = 'red', main = "Normal Quantile Plots of Residuals", ylab = ylabs)
     if (stdres == F){
@@ -126,30 +123,5 @@ matrixPlot <- function (R, histogram = TRUE, method = c("pearson", "kendall",
   do.call(pairs, pairs_args)
   p <- recordPlot()
   return(invisible(p))
-}
-
-
-
-
-resPlots <- function(model, label){
-  
-  #Normal quantile plot of studentized residuals
-  qqp(rstandard(model), pch = 19, main = paste("NQ Plot of Standardized Residuals,", label))
-  
-  #plot of fitted vs. studentized residuals
-  plot(rstandard(model) ~ model$fitted.values, 
-       pch = 19, 
-       col = 'red', 
-       xlab = "Fitted Values", 
-       ylab = "Studentized Residuals",
-       main = paste("Fits vs. Studentized Residuals,", label))
-  abline(h = 0, lwd = 3)
-  abline(h = c(2,-2), lty = 2, lwd = 2, col="blue")
-  abline(h = c(3,-3), lty = 2, lwd = 2, col="green")
-  
-  #Plot of residuals vs. order of the data
-  plot(model, which = 4, lwd = 3, col = 'red')
-  abline(h = 6/length(model$residuals), col = 'black', lwd = 3, lty = 2)
-  
 }
 
